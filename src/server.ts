d770de0bfed146ce9e764070aee6acd60e98f66c
= <=>
2023-12-18 13:06:20 +0530

    fix: logs

--- src/server.ts
+++ src/server.ts
@@ -13,15 +13,11 @@ const server = new Server(5000, {
 })
 
 server.on('connection', (socket) => {
-    console.log('Welcome to med assistance server your socket id is', socket.id)
-
     socket.on('chat-data', (input) => {
         if (!Array.isArray(input)) {
             TwoWayMap.set(socket.id, input)
-            console.log('Returning chat Data for id', input)
             socket.emit('chat-data', chatHistory.get(input))
         } else {
-            console.log('Recieved chat data')
             chatHistory.set(socket.id, input)
         }
     })
@@ -29,10 +25,6 @@ server.on('connection', (socket) => {
     socket.on('client', (message) => {
         const from = socket.id
         const to = TwoWayMap.get(from)
-        console.log('FROM - ',from, 'TO - ', to)
         socket.broadcast.to(to ?? '').emit('server', message)
-        console.log(message)
     })
-
-    // socket.emit('server', "Hello client")
 })
